8d79b70823e6f997b4092ad99163c902c1a65093
mzainiulilabshor <45529539+mzainiulilabshor@users.noreply.github.com>
2018-12-09 15:03:22 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main ()
+{   cout << " Mari kita menentukan "<< endl;
+    int x;
+    cout<<" Masukan Bilangan Bulat Yang Akan Di Cek :";
+    cin>>x;
+    if (x<0)
+        cout<<"Itu Bilangan Negatif";
+    if (x>0)
+        cout<<"itu Bilangan Positif";
+getch();
+        return 0;
+}
